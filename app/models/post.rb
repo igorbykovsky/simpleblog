59f0ee8ee5e5eb4cf523f9ab24548eecab4c7f9d
IharBykouski <y.igrik.y@gmail.com>
2018-11-22 19:46:11 +0300

    add upload of images with carrierwave gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true, length: { minimum: 5 }
 
+  mount_uploader :image, ImageUploader
+
   has_many :comments, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_16_170007) do
+ActiveRecord::Schema.define(version: 2018_11_21_182027) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
     t.text "body"
-    t.integer "post_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
@@ -26,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_11_16_170007) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +50,5 @@ ActiveRecord::Schema.define(version: 2018_11_16_170007) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
